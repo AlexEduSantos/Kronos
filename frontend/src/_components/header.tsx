ae00e34395578ebb27aaf17097cde4a5be7e59ef
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-07-04 10:42:17 -0300

    fix: remover bordas do ícone de notificação e ajustar estilo do botão de calendário

--- frontend/src/_components/header.tsx
+++ frontend/src/_components/header.tsx
@@ -10,15 +10,15 @@ const Header = () => {
     <div className="w-full h-20 flex items-center px-6 justify-between">
       <div className="flex items-center gap-4 rounded-full bg-primary/30 p-1">
         <p className="w-full text-center pl-4">{today}</p>
-        <Button variant={"secondary"} className="aspect-square w-10 h-10 rounded-full ">
+        <Button className="aspect-square w-10 h-10 rounded-full ">
           <CalendarDaysIcon />
         </Button>
       </div>
       <div className="flex items-center gap-4">
-        <span className="w-10 h-10 rounded-full border border-primary flex items-center justify-center overflow-hidden">
-          <BellDot size={24} className="stroke-1 " />
+        <span className="w-10 h-10 rounded-full bg-primary text-primary-foreground flex items-center justify-center overflow-hidden">
+          <BellDot size={24} className=" " />
         </span>
-        <span className="w-10 h-10 rounded-full border border-primary flex items-center justify-center overflow-hidden">
+        <span className="w-10 h-10 rounded-full bg-primary text-primary-foreground flex items-center justify-center overflow-hidden">
           <User2Icon size={40} className="stroke-1 " />
         </span>
       </div>
