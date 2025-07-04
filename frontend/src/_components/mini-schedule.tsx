e711b792cb574171fb5d2892355b4d60131423ba
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-07-04 17:30:15 -0300

    fix: ajustar estilo do componente MiniSchedule para melhorar a legibilidade

--- frontend/src/_components/mini-schedule.tsx
+++ frontend/src/_components/mini-schedule.tsx
@@ -3,10 +3,10 @@ import { Progress } from "./ui/progress";
 
 const MiniSchedule = () => {
   return (
-    <div className="w-full h-44 px-6 flex flex-col gap-2">
+    <div className="w-full px-6 flex flex-col gap-2">
       <div className="w-full h-full border border-primary rounded-md p-4 flex flex-col gap-2 justify-between bg-primary text-primary-foreground">
         <div className="w-full  flex items-center justify-between">
-          <h2 className="text-lg">TJ-PR</h2>
+          <h2 className="text-lg font-bold">TJ-PR</h2>
           <Button
             variant={"secondary"}
             className="h-8 px-4 text-xs"
@@ -14,8 +14,8 @@ const MiniSchedule = () => {
             Editar
           </Button>
         </div>
-        <div>
-          <div className="w-full flex justify-between items-end">
+        <div className="flex flex-col gap-1">
+          <div className="w-full flex justify-between items-end text-sm font-light">
             <p>Progresso</p>
             <small>50%</small>
           </div>
@@ -24,7 +24,7 @@ const MiniSchedule = () => {
             className=""
           />
         </div>
-        <div className="w-full flex gap-2">
+        <div className="w-full flex gap-2 text-sm">
           <h2 className="font-semibold">Data da prova:</h2>
           <p>28 de Agosto de 2025</p>
         </div>
