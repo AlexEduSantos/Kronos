b01e5a96635ec86c5a4817047bc588b28bda08f3
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-07-11 16:37:57 -0300

    refactor: atualizar o componente Header para melhorar a estrutura e a apresentação

--- frontend/src/_components/header.tsx
+++ frontend/src/_components/header.tsx
@@ -1,27 +1,24 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { BellDot, CalendarDaysIcon, MenuIcon, User2Icon } from "lucide-react";
-import { Button } from "./ui/button";
 
 const Header = () => {
   const today = format(new Date(), `dd ' de ' MMMM`, { locale: ptBR });
 
   return (
-    <div className="w-full h-20 flex items-center px-6 justify-between">
-      <div className="flex items-center gap-4 rounded-full bg-primary/30 p-1">
-        <p className="w-full text-center pl-4">{today}</p>
-        <Button className="aspect-square w-10 h-10 rounded-full ">
-          <CalendarDaysIcon />
-        </Button>
-      </div>
+    <div className="w-full flex items-center px-6 justify-between bg-white py-3 fixed top-0 z-50">
       <div className="flex items-center gap-4">
-        <span className="w-10 h-10 rounded-full bg-primary text-primary-foreground flex items-center justify-center overflow-hidden">
-          <BellDot size={24} className=" " />
-        </span>
-        <span className="w-10 h-10 rounded-full bg-primary text-primary-foreground flex items-center justify-center overflow-hidden">
-          <User2Icon size={40} className="stroke-1 " />
-        </span>
+        <div className="w-12 h-12 rounded-full border border-solid text-primary-foreground flex items-center justify-center overflow-hidden">
+          <User2Icon size={40} className="stroke-1"/>
+        </div>
+        <div className="leading-none">
+          <span className="text-md text-primary-foreground leading-none">Olá,</span>
+          <h1 className="text-lg font-semibold text-primary-foreground leading-none">Alex</h1>
+        </div>
       </div>
+      <span className="w-10 h-10 rounded-full text-primary-foreground flex items-center justify-center overflow-hidden">
+        <BellDot size={24} className=" "/>
+      </span>
     </div>
   );
 };
