fae92288df634c6d86d0bd39d4e2155a79cbe493
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-07-15 17:29:48 -0300

    feat: adicionar seleção de data com popover e calendário no agendamento de provas

--- frontend/src/app/(pages)/new-schedule/page.tsx
+++ frontend/src/app/(pages)/new-schedule/page.tsx
@@ -1,14 +1,39 @@
 "use client";
+import { Button } from "@/_components/ui/button";
 import { Calendar } from "@/_components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/_components/ui/popover";
 import { Separator } from "@/_components/ui/separator";
+import { isToday } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { useState } from "react";
+import { ChevronDownIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const NewSchedule = () => {
   const [selectedWeekday, setSelectedWeekday] = useState<string[]>([]);
+  const [testDay, setTestDay] = useState(() => {
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    return date;
+  });
+  const [studyRange, setStudyRange] = useState<{ from: Date; to: Date }>({
+    from: new Date(),
+    to: testDay,
+  });
+  const [open, setOpen] = useState(false);
   const weeakDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
 
-  console.log(selectedWeekday);
+  useEffect(() => {
+    if (testDay) {
+      setStudyRange({
+        from: new Date(),
+        to: testDay,
+      });
+    }
+  }, [testDay]);
 
   return (
     <div className="flex flex-col items-center justify-center p-2">
@@ -20,12 +45,49 @@ const NewSchedule = () => {
             type="text"
             placeholder="Nome do Cronograma"
           />
-          <input
-            className="bg-white rounded-md w-full p-4 appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
-            id="name"
-            type="text"
-            placeholder="Data da prova"
-          />
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                id="date"
+                className="w-full justify-between font-normal bg-white border-none min-h-14 text-primary-foreground"
+              >
+                {testDay ? testDay.toLocaleDateString() : "Select date"}
+                <ChevronDownIcon />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              className="w-full overflow-hidden p-0 border-none shadow-xl "
+              align="end"
+              draggable="true"
+            >
+              <Calendar
+                mode="single"
+                required
+                disabled={(date) => {
+                  return isToday(date) || date < new Date();
+                }}
+                locale={ptBR}
+                selected={testDay}
+                onSelect={(date) => {
+                  setTestDay(date);
+                  setOpen(false);
+                }}
+                captionLayout="dropdown"
+                classNames={{
+                  month_caption: "text-secondary/70 capitalize",
+                  months_dropdown: "text-primary-foreground capitalize",
+                  week: "w-full flex justify-between items-center gap-2",
+                  weekday: "w-full capitalize text-xs",
+                  day: "w-full  flex items-center justify-center rounded-full p-2",
+                  today: "rounded-full bg-primary text-primary-foreground",
+                  outside: "text-muted-foreground/50",
+                  selected: "bg-primary text-primary-foreground rounded-full",
+                }}
+                className="w-full p-2 "
+              />
+            </PopoverContent>
+          </Popover>
           <textarea
             className="bg-white rounded-md w-full p-4 appearance-none focus:outline-none focus:ring-2 focus:ring-primary"
             id="name"
@@ -61,6 +123,7 @@ const NewSchedule = () => {
             <Calendar
               mode="range"
               locale={ptBR}
+              selected={studyRange}
               excludeDisabled={true}
               captionLayout="dropdown"
               classNames={{
