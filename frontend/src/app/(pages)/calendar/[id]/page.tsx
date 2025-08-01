7a4d4a8182d8bc0fde7b02758cd6c663b0c0104d
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-01 17:09:53 -0300

    feat: adicionar botões de ação e melhorar a exibição das disciplinas no cronograma

--- frontend/src/app/(pages)/calendar/[id]/page.tsx
+++ frontend/src/app/(pages)/calendar/[id]/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { Button } from "@/_components/ui/button";
 import { Card, CardContent, CardTitle } from "@/_components/ui/card";
 import { Checkbox } from "@/_components/ui/checkbox";
+import { Label } from "@/_components/ui/label";
 import { Progress } from "@/_components/ui/progress";
 import { mockData } from "@/_mock/data";
 import { format } from "date-fns";
+import { PenIcon, Trash2Icon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
@@ -36,7 +39,7 @@ const ScheduleDetails = () => {
 
   return (
     <Card className="border-none shadow-none p-4 flex flex-col gap-2">
-      <CardTitle className="text-xl">{schedule.name}</CardTitle>
+      <CardTitle className="text-xl text-nowrap">{schedule.name}</CardTitle>
       <div className="flex items-center justify-between gap-2">
         <Progress value={progress} />
         <p className="text-sm">{progress}%</p>
@@ -46,22 +49,52 @@ const ScheduleDetails = () => {
           Data da prova: {format(new Date(schedule.testDay), "dd/MM/yyyy")}
         </p>
         <div className="flex flex-col gap-2">
+          <div className="flex items-center justify-between text-sm text-primary-foreground font-bold gap-2 ">
+            <div className="w-10">Status</div>
+            <div className="w-full">Disciplina</div>
+            <div className="w-8">Peso</div>
+            <div className="w-8">Ação</div>
+          </div>
           {schedule.disciplines.map((discipline, index) => (
             <div
               key={index}
-              className={`text-sm flex items-center justify-between ${
+              className={`text-sm flex items-center gap-2 ${
                 discipline.checked ? "line-through text-muted-foreground" : ""
               }`}
             >
-              <div className="flex gap-2 items-center">
-                <Checkbox
-                  checked={discipline.checked}
-                  onCheckedChange={() => toggleDiscipline(index)}
-                  className="border border-border"
-                />
-                <p>{discipline.name}</p>
+              <div className="flex gap-2 items-center min-w-[260px] w-full">
+                <div className="w-10 flex items-center justify-center">
+                  <Checkbox
+                    id={`discipline-${index}`}
+                    checked={discipline.checked}
+                    onCheckedChange={() => toggleDiscipline(index)}
+                    className="border border-border"
+                  />
+                </div>
+                <Label
+                  htmlFor={`discipline-${index}`}
+                  className="line-clamp-1 font-normal"
+                >
+                  {discipline.name}
+                </Label>
               </div>
-              <p>Peso: {discipline.weight}</p>
+              <p className="w-8 flex items-center justify-center font-semibold">{discipline.weight}</p>
+              <div className="flex items-center justify-center gap-1 w-fit">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="p-0 m-0 w-fit h-fit"
+                >
+                  <Trash2Icon size={16} className="stroke-1 text-red-500" />
+                </Button>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="p-0 m-0 w-fit h-fit"
+                >
+                  <PenIcon size={16} className="stroke-1 text-primary-foreground" />
+                </Button>
+              </div>
             </div>
           ))}
         </div>
