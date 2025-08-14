d775b643f78342879dca007439ca4d60302b961d
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-14 17:25:25 -0300

    refactor: remover módulos e serviços do Prisma e controlador de aplicação

--- backend/src/_prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService], // Exporta o serviço para que outros módulos possam usá-lo
-})
-export class PrismaModule {}
