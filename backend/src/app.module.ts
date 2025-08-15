ca387765ca5cfffeb6852e77fe88fd125284c05c
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-15 16:37:39 -0300

    feat: adicionar módulo de autenticação com controlador e serviço

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from './schedule/schedule.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ScheduleModule],
+  imports: [ScheduleModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
