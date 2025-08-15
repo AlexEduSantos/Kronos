a2bf60718d7c0d6d435843a13770650a1e38ec6b
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-15 17:30:09 -0300

    feat: adicionar DTO e serviço de usuário com métodos para criação e busca

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,31 @@
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CreateUserDto } from './dtos/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async createUser(data: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name ?? '',
+        password: hashedPassword,
+        avatar: 'default-avatar.jpg',
+      },
+    });
+  }
+}
