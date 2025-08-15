32e29d369cfa01ea91449e2e8dd225db4cb5c8ff
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-15 17:11:30 -0300

    feat: adicionar módulo de usuário com controlador e serviço

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from './schedule/schedule.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ScheduleModule, AuthModule],
+  imports: [ScheduleModule, AuthModule, UserModule],
   controllers: [],
   providers: [PrismaService],
 })
