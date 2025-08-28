66f14c2b7e638f03190fdccf21cacf7dbc5c36ac
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-08-28 17:02:38 -0300

    feat: add User data

--- frontend/src/_components/header.tsx
+++ frontend/src/_components/header.tsx
@@ -1,23 +1,45 @@
+"use client";
+import { useUser } from "@/_viewmodels/useUser";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { BellDot, CalendarDaysIcon, MenuIcon, User2Icon } from "lucide-react";
+import { BellDot, User2Icon } from "lucide-react";
+import Image from "next/image";
 
 const Header = () => {
-  const today = format(new Date(), `dd ' de ' MMMM`, { locale: ptBR });
+  const { data, isLoading, isError, error } = useUser();
+
+  if (isLoading) {
+    return <div>Carregando dados do usuário...</div>;
+  }
+
+  if (isError) {
+    console.error("Erro ao carregar dados do usuário:", error);
+    return <div>Erro ao carregar dados. Por favor, tente novamente.</div>;
+  }
+
+  console.log("Dados do usuário:", data);
 
   return (
     <div className="w-full flex items-center px-6 justify-between bg-white py-3 fixed top-0 z-50">
       <div className="flex items-center gap-4">
-        <div className="w-12 h-12 rounded-full border border-solid text-primary-foreground flex items-center justify-center overflow-hidden">
-          <User2Icon size={40} className="stroke-1"/>
+        <div className="w-12 h-12 rounded-full border border-solid text-primary-foreground flex items-center justify-center overflow-hidden relative">
+          <Image
+            src={data?.avatar}
+            alt="Avatar"
+            className="w-full h-full object-cover"
+            fill
+            loading="eager"
+          />
         </div>
         <div className="leading-none">
-          <span className="text-md text-primary-foreground leading-none">Olá,</span>
-          <h1 className="text-lg font-semibold text-primary-foreground leading-none">Alex</h1>
+          <span className="text-md text-primary-foreground leading-none">
+            Olá, {data?.name}
+          </span>
+          <h1 className="text-lg font-semibold text-primary-foreground leading-none"></h1>
         </div>
       </div>
       <span className="w-10 h-10 rounded-full text-primary-foreground flex items-center justify-center overflow-hidden">
-        <BellDot size={24} className=" "/>
+        <BellDot size={24} className=" " />
       </span>
     </div>
   );
