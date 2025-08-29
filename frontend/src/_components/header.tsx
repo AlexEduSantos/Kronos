af2e797e940c6388762dd9c46d292a33c7f438d5
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-08-28 22:43:13 -0300

    feat: add skeleton

--- frontend/src/_components/header.tsx
+++ frontend/src/_components/header.tsx
@@ -2,29 +2,29 @@
 import { useUser } from "@/_viewmodels/useUser";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { BellDot, User2Icon } from "lucide-react";
+import { BellDot, Loader2, User2Icon } from "lucide-react";
 import Image from "next/image";
+import { Skeleton } from "./ui/skeleton";
 
 const Header = () => {
-  const { data, isLoading, isError, error } = useUser();
+  const { userData: user, isLoading, isError, error } = useUser();
 
-  if (isLoading) {
-    return <div>Carregando dados do usuário...</div>;
+  if (isLoading || isError || user === undefined) {
+    return (
+      <div className="w-full flex items-center justify-between bg-white fixed top-0 z-50">
+        <Skeleton className="w-full h-16 bg-background/20 rounded-none flex items-center justify-center">
+          <Loader2 className="w-6 h-6 animate-spin opacity-20" />
+        </Skeleton>
+      </div>
+    );
   }
-
-  if (isError) {
-    console.error("Erro ao carregar dados do usuário:", error);
-    return <div>Erro ao carregar dados. Por favor, tente novamente.</div>;
-  }
-
-  console.log("Dados do usuário:", data);
 
   return (
     <div className="w-full flex items-center px-6 justify-between bg-white py-3 fixed top-0 z-50">
       <div className="flex items-center gap-4">
         <div className="w-12 h-12 rounded-full border border-solid text-primary-foreground flex items-center justify-center overflow-hidden relative">
           <Image
-            src={data?.avatar}
+            src={user?.avatar}
             alt="Avatar"
             className="w-full h-full object-cover"
             fill
@@ -33,7 +33,7 @@ const Header = () => {
         </div>
         <div className="leading-none">
           <span className="text-md text-primary-foreground leading-none">
-            Olá, {data?.name}
+            Olá, {user?.name}
           </span>
           <h1 className="text-lg font-semibold text-primary-foreground leading-none"></h1>
         </div>
