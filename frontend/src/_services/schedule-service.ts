aa6ff4615b8d821161a8a283fa19a39c29f25433
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-09-15 17:16:52 -0300

    feat: add schedule service

---
+++ frontend/src/_services/schedule-service.ts
@@ -0,0 +1,207 @@
+import { apiRoute } from "@/_lib/utils";
+
+// ======================
+// SCHEDULE
+// ======================
+export async function getAllSchedules() {
+  try {
+    const response = await fetch(apiRoute + "/schedule", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    if (!response.ok) {
+      if (response.status === 401) {
+        throw new Error("Sessão expirada. Por favor, faça login novamente.");
+      }
+      if (response.status === 404) {
+        throw new Error("Nenhum cronograma encontrado.");
+      }
+      throw new Error("Falha ao buscar cronogramas do usuário.");
+    }
+    return await response.json();
+  } catch (error) {
+    throw new Error("Falha ao buscar cronogramas do usuário.");
+  }
+}
+
+export async function getScheduleById(scheduleId: string) {
+  const response = await fetch(`${apiRoute}/schedule/${scheduleId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) throw new Error("Falha ao buscar agendamento.");
+  return await response.json();
+}
+
+export async function createSchedule(data: any) {
+  const response = await fetch(`${apiRoute}/schedule/createSchedule`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao criar agendamento.");
+  return await response.json();
+}
+
+export async function updateSchedule(scheduleId: string, data: any) {
+  const response = await fetch(`${apiRoute}/schedule/${scheduleId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao atualizar agendamento.");
+  return await response.json();
+}
+
+export async function deleteSchedule(scheduleId: string) {
+  const token = localStorage.getItem("access_token");
+  if (!token) throw new Error("Token de acesso ausente.");
+
+  const response = await fetch(`${apiRoute}/schedule/${scheduleId}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (!response.ok) throw new Error("Falha ao deletar agendamento.");
+}
+
+// ======================
+// DAY
+// ======================
+export async function getDayById(dayId: string) {
+  const response = await fetch(`${apiRoute}/schedule/days/${dayId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) throw new Error("Falha ao buscar dia.");
+  return await response.json();
+}
+
+export async function createDay(scheduleId: string, data: any) {
+  const response = await fetch(`${apiRoute}/schedule/${scheduleId}/days`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao criar dia.");
+  return await response.json();
+}
+
+export async function updateDay(dayId: string, data: any) {
+  const response = await fetch(`${apiRoute}/schedule/days/${dayId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao atualizar dia.");
+  return await response.json();
+}
+
+export async function deleteDay(dayId: string) {
+  const response = await fetch(`${apiRoute}/schedule/days/${dayId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) throw new Error("Falha ao deletar dia.");
+}
+
+// ======================
+// TOPIC
+// ======================
+export async function getTopicById(topicId: string) {
+  const response = await fetch(`${apiRoute}/schedule/topics/${topicId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) throw new Error("Falha ao buscar tópico.");
+  return await response.json();
+}
+
+export async function createTopic(dayId: string, data: any) {
+  const response = await fetch(`${apiRoute}/schedule/days/${dayId}/topics`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao criar tópico.");
+  return await response.json();
+}
+
+export async function updateTopic(topicId: string, data: any) {
+  const response = await fetch(`${apiRoute}/schedule/topics/${topicId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) throw new Error("Falha ao atualizar tópico.");
+  return await response.json();
+}
+
+export async function toggleStatusTopic(topicId: string) {
+  const response = await fetch(
+    `${apiRoute}/schedule/topics/${topicId}/status`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ topicId: topicId }),
+    }
+  );
+}
+
+export async function deleteTopic(topicId: string) {
+  const response = await fetch(`${apiRoute}/schedule/topics/${topicId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) throw new Error("Falha ao deletar tópico.");
+}
