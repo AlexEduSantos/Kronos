1add14381d75cfe54fb97384b49b7c991c7e3b08
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-09-16 15:59:51 -0300

    feat: traduzir nomes de links no cabeçalho para português

--- frontend/src/_components/header.tsx
+++ frontend/src/_components/header.tsx
@@ -8,16 +8,26 @@ const Header = () => {
 
   const link = [
     { name: "Home", href: "/" },
-    { name: "Schedules", href: "/schedules" },
-    { name: "Settings", href: "/settings" },
+    { name: "Cronogramas", href: "/schedules" },
+    { name: "Configurações", href: "/settings" },
   ];
+
+  let currentLinkName = "Título";
+  if (pathname === "/") {
+    currentLinkName = "Home";
+  } else {
+    const found = link.find(
+      (item) => pathname.startsWith(item.href) && item.href !== "/"
+    );
+    if (found) {
+      currentLinkName = found.name;
+    }
+  }
 
   return (
     <div className="w-full h-20 bg-white py-2 px-6 flex justify-between items-center text-primary-foreground">
       <ChevronLeft className="h-6 w-6" onClick={() => router.back()} />
-      <h2 className="text-2xl font-bold text-center">
-        {link.find((item) => item.href === pathname)?.name}
-      </h2>
+      <h2 className="text-2xl font-bold text-center">{currentLinkName}</h2>
       <Search className="h-6 w-6" />
     </div>
   );
