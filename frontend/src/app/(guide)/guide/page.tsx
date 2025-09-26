8407b39242cceb34cde57269893a33827cd02a9d
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-09-26 09:14:11 -0300

    feat: add guide page

---
+++ frontend/src/app/(guide)/guide/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/_components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const GuidePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-around h-screen py-12">
+      <div className="relative w-full aspect-square ">
+        <Image
+          src="/guide.png"
+          alt="Guide Image"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="w-full flex flex-col items-center">
+        <h2 className="text-2xl text-center">
+          Olá, <strong>Usuário.</strong>
+        </h2>
+        <p className="text-center w-2/3">
+          Você não possui nenhum cronograma de estudos ativo.
+          <br />
+          Vamos cadastrar um juntos!
+        </p>
+      </div>
+      <Link href="/new-schedule" className="w-full flex justify-center">
+        <Button className="text-lg w-2/4 h-12 font-bold">Cadastrar</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default GuidePage;
